eb62d7520d2ba2a068bb854316087f383d06ee12
spaceman75 <bladerunner9040@gmail.com>
2023-11-09 21:20:50 -0300

    chore: unused files removed

--- projects/spring-boot/java/base/src/main/resources/db/migration/V1__create-user-table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `users` (
-	`id` BIGINT NOT NULL AUTO_INCREMENT,
-	`first_name` VARCHAR(255) NOT NULL,
-	`last_name` VARCHAR(255) NOT NULL,
-	`age` INT NOT NULL,
-	`email` VARCHAR(255) NOT NULL,
-	`is_active` BIT(1) NOT NULL,
-	PRIMARY KEY (`id`)
-);
